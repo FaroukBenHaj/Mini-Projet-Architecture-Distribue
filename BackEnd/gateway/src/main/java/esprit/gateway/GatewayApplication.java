23cb470c21a1083019c0aa43b120d20f0ee9063a
NadaLouhichi <nada.louhichi@esprit.tn>
2025-04-07 08:51:11 +0100

    Better Configuration Gateway

--- BackEnd/gateway/src/main/java/esprit/gateway/GatewayApplication.java
+++ BackEnd/gateway/src/main/java/esprit/gateway/GatewayApplication.java
@@ -32,9 +32,9 @@ public class GatewayApplication {
 				.route("Hebergement_Domain",
 						r -> r.path("/Hebergement_Domain/**")
 								.uri("lb://Hebergement_Domain"))
-				.route("Universite-Foyer",
-						r->r.path("/Universite-Foyer/**")
-								.uri("lb://Universite-Foyer"))
+				.route("universite-foyer", r -> r.path("/Universite-Foyer/**")
+						.uri("lb://UNIVERSITE-FOYER"))
+
 				.build();
 
 	}
