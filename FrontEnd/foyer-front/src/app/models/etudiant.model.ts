54f6fdb7fe3914e528d32253de6cb480e0bb6cb9
FaroukBenhaj <mohamedfarouk.benhajamor@esprit.tn>
2025-04-16 12:27:38 +0100

    Updated configurations, removed unused files, and added new services

--- FrontEnd/foyer-front/src/app/models/etudiant.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Etudiant {
-    idEtudiant?: number;
-    nomEt: string;
-    prenomEt: string;
-    cin: number;
-    ecole: string;
-    dateNaissance: string; // Changé en string pour correspondre au format ISO
-    reservations?: any[]; // Ajouté pour gérer les relations
-  }
--- FrontEnd/foyer-front/src/app/models/payment.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Payment {
-    idPayment?: number;
-    montant: number;
-    datePaiement: string;
-    status: string;
-  }
-  
